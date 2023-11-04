571584017247a1dcefe16926e6099658f4570e91
Mateo Rodon <mateorodon123@gmail.com>
2023-11-04 09:09:15 -0300

    Modifica version java y pom.xml

--- src/main/java/AnalisisLexico/AccionesSemanticas/AS0.java
+++ src/main/java/AnalisisLexico/AccionesSemanticas/AS0.java
@@ -13,16 +13,26 @@ public class AS0 extends AccionSemantica{
     @Override
     public void ejecutar(Token t, Reader entrada) {
         switch (t.getLexema()) {
-            case "!!" -> t.setId(DIST);
-            case "+=" -> t.setId(ASIG);
-            case "<=" -> t.setId(MENORIGUAL);
-            case ">=" -> t.setId(MAYORIGUAL);
-            case "==" -> t.setId(IGUAL);
-            default -> {
-                int cod_ascii = (int) t.getLexema().charAt(0); //Obtenemos el cod ASCII
+            case "!!":
+                t.setId(DIST);
+                break;
+            case "+=":
+                t.setId(ASIG);
+                break;
+            case "<=":
+                t.setId(MENORIGUAL);
+                break;
+            case ">=":
+                t.setId(MAYORIGUAL);
+                break;
+            case "==":
+                t.setId(IGUAL);
+                break;
+            default:
+                int cod_ascii = (int) t.getLexema().charAt(0); // Obtenemos el código ASCII
                 t.setId(cod_ascii);
-            }
         }
+    }
     }
 
 }
